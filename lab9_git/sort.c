1bb8f17b72b668b2fc0dbc61f168de8d87defee8
George Kolesov <george.kolesov12@yandex.ru>
2025-12-02 17:43:02 +0700

    Добавлены комментарии

--- lab9_git/sort.c
+++ lab9_git/sort.c
@@ -5,7 +5,7 @@ void swap(int* arr, int i, int j) {
     arr[i] = arr[j];
     arr[j] = temp;
 }
-
+// Функция сортировки пузырьком
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
       
