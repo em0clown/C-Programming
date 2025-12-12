a39578bb0dffccb74edd03c837e22409bd47cb39
Георгий <george.kolesov12@yandex.ru>
2025-12-12 13:20:39 +0700

    код по 10 лаб

--- lab10_struct/var13.c
+++ lab10_struct/var13.c
@@ -3,14 +3,30 @@
 #include <math.h>
 
 struct parabola{
-    int a, b, c;
+    double a, b, c;
+};
+
+int Min(struct parabola p){
+    return p.a>0;
 }
 
+double calcMin(struct parabola p){
+    return p.c-(p.b*p.b)/(4 * p.a);
+}
 
 int main(){
-    int M, a, b, c;
-    printf("Введите кол-во парабол (M): ");
-    scanf("%d", &M);
-    printf("Введите параметры параболы (a, b, c): ");
-    scanf("%d, %d, %d", &a, &b, &c);
+    struct parabola test1 = {1.0, 0.0, 0.0};
+    struct parabola test2 = {-1.0, 0.0, 0.0};
+
+    printf("Тест 1 - y=x^2: ");
+    printf("%d\n", Min(test1));
+    if (Min(test1)){
+        printf("%2f\n", calcMin(test1));
+    }
+
+    printf("Тест 2 - y=-x^2: ");
+    printf("%d\n", Min(test2));
+    if (Min(test2)){
+        printf("%2f\n", calcMin(test2));
+    }
 }
