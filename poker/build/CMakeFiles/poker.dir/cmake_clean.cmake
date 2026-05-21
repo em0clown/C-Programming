cd3cac1a8b3468cec594ab592e8769c249dc6b79
George Kolesov <george.kolesov12@yandex.ru>
2026-05-22 00:23:21 +0700

    test pocker

---
+++ poker/build/CMakeFiles/poker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poker.dir/src/poker.cpp.o"
+  "CMakeFiles/poker.dir/src/poker.cpp.o.d"
+  "poker"
+  "poker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
